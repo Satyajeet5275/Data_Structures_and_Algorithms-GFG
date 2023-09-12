6a10969473b5b1fc77dd50e34682a8f8e99f7804
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-09-12 23:25:40 +0530

    Updated Solution - GfG to GitHub

--- Easy/Perfect Numbers/perfect-numbers.cpp
+++ Easy/Perfect Numbers/perfect-numbers.cpp
@@ -3,51 +3,31 @@
 using namespace std;
 
 // } Driver Code Ends
-// class Solution {
-//   public:
-//     long long factorsOf(long long n)
-//     {
-//         long long sum=1;
-//         for(int i=2;i*i<=n;i++)
-//         {
-//             if(n%i==0) 
-//             {
-//                 sum+=i;
-//                 if(n/i!=i)
-//                     sum+=n/i;
-//             }
-//         }
-//         return sum;
-//     }
-//     int isPerfectNumber(long long N) {
-//         // code here
-//         if(N==1) return 0;
-//         int factSum=factorsOf(N);
-//         cout<<factSum;
-//         return (N==factSum);
-//     }
-// };
 class Solution {
   public:
-    int isPerfectNumber(long long N) {
-        // code here
-        if(N==1)
-        return 0;
+    long long factorsOf(long long n)
+    {
         long long sum=1;
-        for(long long i=2;i*i<=N;i++)
+        for(long long i=2;i*i<=n;i++)
         {
-            if(N%i==0)
+            if(n%i==0) 
             {
                 sum+=i;
-                if(N/i!=i)
-                sum+=N/i;
+                if(n/i!=i)
+                    sum+=n/i;
             }
         }
-        if(sum==N)
-        return 1;
-        return 0;
+        return sum;
+    }
+    int isPerfectNumber(long long N) {
+        // code here
+        if(N==1) return 0;
+        long long factSum=factorsOf(N);
+        // cout<<factSum;
+        return (N==factSum);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
