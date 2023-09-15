97ec66918823ba49bd7b3248edd08f90222169c0
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-09-15 18:29:36 +0530

    Updated Solution - GfG to GitHub

--- Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -10,19 +10,36 @@ using namespace std;
 class Solution{
 public:
 
-    bool targetSum(int arr[], int n, int target) {
-        vector<int> dp(target + 1);
-        dp[0] = true;
-        for(int i = 0; i < n; i++) 
-            for(int t = target; t >= arr[i]; t--)
-                dp[t] |= dp[t - arr[i]];
-        return dp.back();
+     bool solve(int index, int target, vector<vector<int>> &dp, int arr[]){
+         if(target == 0){
+             return 1;
+         }
+         if(target < 0 || index < 0){
+             return 0;
+         }
+         if(dp[index][target] != -1){
+             return dp[index][target];
+         }
+         
+         bool nontake = solve(index-1, target, dp, arr);
+         bool take = solve(index-1, target-arr[index], dp, arr);
+         
+         return dp[index][target] = take | nontake;
     }
-
-    int equalPartition(int n, int arr[]) {
-        int sum = accumulate(arr, arr + n, 0);
-        if(sum & 1) return false;
-        return targetSum(arr, n, sum / 2);
+    
+    int equalPartition(int N, int arr[])
+    {
+        int sum = 0;
+        for(int i=0; i<N; i++){
+            sum = sum + arr[i];
+        }
+        if(sum%2==1){
+            return 0;
+        }
+        
+        int target = sum/2;
+        vector<vector<int>> dp(N, vector<int>(target+1, -1));
+        return solve(N-1, target, dp, arr);
     }
 };
 
