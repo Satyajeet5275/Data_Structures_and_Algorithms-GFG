29dc7fa165eeae9aca34a8ed92b51339f014beff
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-10-01 13:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Eventual Safe States/eventual-safe-states.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> eventualSafeNodes(int N, vector<int> adj1[]) {
+        // code here
+        vector<vector<int>> adj(N);
+	    vector<int> indegree(N,0);
+	    for(int i=0;i<N;i++){
+            for(auto x:adj1[i]){
+                adj[x].push_back(i);
+                indegree[i]++;
+            }
+        }
+	    queue<int> q;
+	    for(int i=0;i<N;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+
+	    vector<int> ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto x:adj[node]){
+	            indegree[x]--;
+	            if(indegree[x]==0){
+	                q.push(x);
+	            }
+	        }
+	    }
+        sort(ans.begin(),ans.end());
+	    return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+        for (auto i : safeNodes) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
