2ab6895b0f236bffb1793c73a02896c05b7827a8
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-10-25 23:18:37 +0530

    Updated Solution - GfG to GitHub

--- Hard/Making A Large Island/making-a-large-island.cpp
+++ Hard/Making A Large Island/making-a-large-island.cpp
@@ -4,113 +4,122 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-class DisjointSet {
-
-public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
-class Solution {
-private:
-    bool isValid(int newr, int newc, int n) {
-        return newr >= 0 && newr < n && newc >= 0 && newc < n;
-    }
-public:
-    int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        DisjointSet ds(n * n);
-        // step - 1
-        for (int row = 0; row < n ; row++) {
-            for (int col = 0; col < n ; col++) {
-                if (grid[row][col] == 0) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n) && grid[newr][newc] == 1) {
-                        int nodeNo = row * n + col;
-                        int adjNodeNo = newr * n + newc;
-                        ds.unionBySize(nodeNo, adjNodeNo);
-                    }
-                }
+class DisjointSet
+{
+    public:
+        vector<int> parent;
+        vector<int> rank;
+        vector<int> size;
+        DisjointSet(int n)
+        {
+            rank.resize(n+1,0);
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++)
+            {
+                parent[i]=i;
             }
         }
-        // step 2
-        int mx = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 1) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                set<int> components;
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n)) {
-                        if (grid[newr][newc] == 1) {
-                            components.insert(ds.findUPar(newr * n + newc));
+        int find_parent(int node)
+        {
+            if(parent[node]==node)
+                return node;
+            return parent[node]=(find_parent(parent[node]));
+        }
+        void union_by_rank(int u,int v)
+        {
+            int pv = find_parent(v);
+            int pu = find_parent(u);
+            if(pv==pu)  
+                return;
+            if(rank[pv]<rank[pu])
+            {
+                parent[pv]=pu;
+            }
+            else if(rank[pv]>rank[pu])
+            {
+                parent[pu]=pv;
+            }
+            else
+            {
+                parent[pv]=pu;
+                rank[pu]++;
+            }
+        }
+        void union_by_size(int u,int v)
+        {
+            int pv = find_parent(v);
+            int pu = find_parent(u);
+            if(pv==pu)  
+                return;
+
+            if(size[pv]<size[pu])  {
+                parent[pv]=pu;
+                size[pu]+=size[pv];
+            }
+            else {  
+                parent[pu]=pv;
+                size[pv]+=size[pu];
+            }
+        }
+};
+
+class Solution
+{
+public:
+    int largestIsland(vector<vector<int>>& grid) 
+    {
+        int n=grid.size();
+        DisjointSet ds(n*n);
+        int dr[]={-1,0,1,0};
+        int dc[]={0,-1,0,1};
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
+                if(grid[r][c]==0) continue;
+                for(int i=0;i<4;i++){
+                    // cout<<"jdjd";
+                    int nr=r+dr[i];
+                    int nc=c+dc[i];
+                    if(nr>=0 && nr<n && nc>=0 && nc<n){
+                        if(grid[nr][nc]==1){
+                        // cout<<" in";
+                            
+                        int node=r*n+c;
+                        int adjNode=nr*n+nc;
+                        ds.union_by_size(node,adjNode);
                         }
                     }
                 }
-                int sizeTotal = 0;
-                for (auto it : components) {
-                    sizeTotal += ds.size[it];
-                }
-                mx = max(mx, sizeTotal + 1);
             }
         }
-        for (int cellNo = 0; cellNo < n * n; cellNo++) {
-            mx = max(mx, ds.size[ds.findUPar(cellNo)]);
+        int mx=0;
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
+                if(grid[r][c]==1) continue;
+                set<int> comp;
+                for(int i=0;i<4;i++){
+                    int nr=r+dr[i];
+                    int nc=c+dc[i];
+                    if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==1){
+                        comp.insert(ds.find_parent(nr*n+nc));
+                    }
+                }
+                int total=0;
+                for(auto x:comp){
+                    total+=ds.size[x];
+                    // cout<<ds.size[x]<< " ";
+                }
+                // cout<<comp.size()<<" sz";
+                mx=max(mx,total+1);
+            }
+        }
+        for(int i=0;i<n*n;i++){
+            mx=max(mx,ds.size[ds.find_parent(i)]);
+            // cout<<ds.size[ds.parent[i]]<<", ";
         }
         return mx;
     }
 };
-
 
 //{ Driver Code Starts.
 
