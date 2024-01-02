fba48ddc987af4cef0f95eeee050af4f800e96b0
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-01-02 21:22:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Sum Subarray of Size at least K/largest-sum-subarray-of-size-at-least-k.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// C++ program to find largest subarray sum with
+// at-least k elements in it.
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:    
+    long long int maxSumWithK(long long int arr[], long long int n, long long int k) 
+    {
+        long long int ans=0,sum=0,prevMax=0;
+        for(int i=0;i<(int)k;i++){
+            sum+=arr[i];
+        }
+        prevMax=sum;
+        ans=sum;
+        int start=0;
+        for(int i=(int)k;i<(int)n;i++){
+            sum+=arr[i]-arr[start++];
+            prevMax=max(prevMax+arr[i],sum);
+            ans=max(ans,prevMax);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    long long int t;
+    cin >> t;
+    while (t--) {
+        long long int n, k, i;
+        cin >> n;
+        long long int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cin >> k;
+        Solution ob;
+        cout << ob.maxSumWithK(a, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
