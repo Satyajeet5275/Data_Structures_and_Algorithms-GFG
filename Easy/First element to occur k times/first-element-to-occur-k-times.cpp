77822273417f2171f5a43644c9bd24b5c540bc84
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-03-04 19:55:10 +0530

    Updated Solution - GfG to GitHub

--- Easy/First element to occur k times/first-element-to-occur-k-times.cpp
+++ Easy/First element to occur k times/first-element-to-occur-k-times.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution{
     public:
     int firstElementKTime(int n, int k, int a[])
     {
-        unordered_map<int,int> ans; // Creation of hashmap
-        for(int i=0;i<n;i++){
-                ans[a[i]]++; // Increase the counter variable for every entry
-                if(ans[a[i]]==k) return a[i]; // first element which reaches k is returned
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++) {
+            mp[a[i]]++;
+            if(mp[a[i]]>=k) return a[i];
         }
-        return -1; // Else -1 is returned 
+        return -1;
     }
 };
 
